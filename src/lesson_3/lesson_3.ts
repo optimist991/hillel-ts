85b223865b58a25a7dac7190412ce0359d2fc1b8
Andy Shulga <optimist991@gmail.com>
2025-03-18 13:00:09 +0000

    Add new homework - so then I can see changes in a PR

---
+++ src/lesson_3/lesson_3.ts
@@ -0,0 +1,29 @@
+// // Union type
+
+// let foo: number | string | boolean = 'hello world';
+// console.log(foo)
+
+// foo = 'goodbye world'
+// console.log(foo)
+
+// foo = 10
+// console.log(foo)
+
+// foo = true
+// console.log(foo)
+
+// class A {
+//     a: number;
+// }
+// class B {
+//     b: string;
+// }
+// class C {
+//     c: boolean;
+// }
+
+
+// let name: A & B & C;
+
+
+
